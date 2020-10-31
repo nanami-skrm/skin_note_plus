57953d2952f87669a7ff105cf3e37166ede63d58
unknown <skrm773@gmail.com>
2020-10-31 18:32:59 +0900

    [Add]note create

--- app/controllers/user/notes_controller.rb
+++ app/controllers/user/notes_controller.rb
@@ -6,8 +6,29 @@ class User::NotesController < ApplicationController
 	def new
 		@note = Note.new
 		@my_items = MyItem.where(user_id: current_user.id)
+		@note.todays_items.build
 	end
 
 	def create
+		params[:note][:user_id] = current_user.id
+		note = Note.new(note_params)
+		if note.save
+			todays_item_params[:my_item_id].each do |my_item_id|
+				TodaysItem.create(note_id: note.id, my_item_id: my_item_id)
+			end
+			redirect_to user_notes_path
+		else
+			@my_items = MyItem.where(user_id: current_user.id)
+			render "new"
+		end
+	end
+
+	def note_params
+		params.require(:note).permit(:user_id, :date, :condition)
+		#params.require(:note).permit(:user_id, :date, :condition, my_item_id: [])
+	end
+
+	def todays_item_params
+        params.require(:todays_items).permit(my_item_id: [])
 	end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -3,5 +3,7 @@ class Note < ApplicationRecord
 	has_many :todays_items, dependent: :destroy
 	belongs_to :user
 
+	validates :date, uniqueness: true
+
     enum condition: { 良い: 0, 普通: 1, 少し悪い: 2, 悪い: 3 }
 end
