def44acea9ac4ffe77fa3113b6b4904993fedbff
unknown <skrm773@gmail.com>
2020-11-06 23:53:17 +0900

    [Add]item index layout

--- app/controllers/user/empathies_controller.rb
+++ app/controllers/user/empathies_controller.rb
@@ -1,10 +1,23 @@
 class User::EmpathiesController < ApplicationController
 
 	def create
-
+		tweet = Tweet.find(params[:tweet_id])
+		empathy = Empathy.new
+		empathy.user_id = current_user.id
+		empathy.tweet_id = params[:tweet_id]
+		empathy.save
+		redirect_to request.referer
 	end
 
 	def destroy
+		tweet = Tweet.find(params[:tweet_id])
+		empathy = current_user.empathies.find_by(tweet_id: tweet.id)
+		empathy.save
+		redirect_to request.referer
 	end
+
+	# def empathy_params
+	# 	params.require(:empathy).permit(:user_id, :tweet_id)
+	# end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
 	has_many :interests, dependent: :destroy
 	has_many :reviews, dependent: :destroy
 
+	validates :item_name, presence: true
+	validates :item_genre, presence: true
+	validates :maker, presence: true
+
 	attachment :image
 
  	enum item_genre: { クレンジング: 0, 洗顔料: 1, 化粧水: 2, 乳液: 3, 美容液: 4, パック: 5, ポイントスペシャルケア: 6, その他: 7 }
