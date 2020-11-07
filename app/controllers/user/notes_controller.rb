9c89504e4cc6b93cb26505622c953092d3ea4a2a
unknown <skrm773@gmail.com>
2020-11-07 15:44:05 +0900

    d\ドーナツグラフのパフォーマンス改善

--- app/controllers/user/notes_controller.rb
+++ app/controllers/user/notes_controller.rb
@@ -4,23 +4,28 @@ class User::NotesController < ApplicationController
 		@beginning_of_month = Date.new(params[:year].to_i, params[:month].to_i, 1)
 		@end_of_month = Date.new(params[:year].to_i, params[:month].to_i, -1)
 		@notes = current_user.notes.where(date: @beginning_of_month..@end_of_month)
-		@excellent_condition_count = current_user.notes.where(date: @beginning_of_month..@end_of_month, condition: 4).count
-		@good_condition_count = current_user.notes.where(date: @beginning_of_month..@end_of_month, condition: 3).count
-		@average_condition_count = current_user.notes.where(date: @beginning_of_month..@end_of_month, condition: 2).count
-		@poor_condition_count = current_user.notes.where(date: @beginning_of_month..@end_of_month, condition: 1).count
-		@bad_condition_count = current_user.notes.where(date: @beginning_of_month..@end_of_month, condition: 0).count
+	
+		# {"悪い"=>13, "少し悪い"=>3, "普通"=>4, "良い"=>4, "とても良い"=>2}
+		grouping_conditions_count = @notes.group(:condition).count
 
-		@condition_list = (@beginning_of_month..@end_of_month).map do |date|
-  		{ x: date, y: @notes.find_by(date: date)&.read_attribute_before_type_cast(:condition) || 2 }
-    	end
-	    # ↑投稿がなかった日の肌のコンディションを「2:普通」にする
 
-	    @todays_items_list = []
-		current_user.my_items.pluck(:id).each do |my_item_id|
-			(@beginning_of_month..@end_of_month).each do |date|
-				@todays_items_list.push({ x: date, y: TodaysItem.joins(:note, :my_item).where(my_items: { user_id: current_user.id }, my_item_id: my_item_id).where('notes.date = ?', date).first&.my_item_id })
-			end #.pushは()内の要素を配列の末尾に追加する .joinsは複数のテーブルにまたがるレコードを条件の一致するものだけくっつける
-		end
+		@excellent_condition_count = grouping_conditions_count["とても良い"]
+		@good_condition_count = grouping_conditions_count["良い"]
+		@average_condition_count = grouping_conditions_count["普通"]
+		@poor_condition_count = grouping_conditions_count["少し悪い"]
+		@bad_condition_count = grouping_conditions_count["悪い"]
+
+		# @condition_list = (@beginning_of_month..@end_of_month).map do |date|
+  # 		{ x: date, y: @notes.find_by(date: date)&.read_attribute_before_type_cast(:condition) || 2 }
+  #   	end
+	 #    # ↑投稿がなかった日の肌のコンディションを「2:普通」にする
+
+	 #    @todays_items_list = []
+		# current_user.my_items.pluck(:id).each do |my_item_id|
+		# 	(@beginning_of_month..@end_of_month).each do |date|
+		# 		@todays_items_list.push({ x: date, y: TodaysItem.joins(:note, :my_item).where(my_items: { user_id: current_user.id }, my_item_id: my_item_id).where('notes.date = ?', date).first&.my_item_id })
+		# 	end #.pushは()内の要素を配列の末尾に追加する .joinsは複数のテーブルにまたがるレコードを条件の一致するものだけくっつける
+		# end
 	end
 
 	def new
