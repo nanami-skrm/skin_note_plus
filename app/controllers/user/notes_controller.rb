0718c5a065369af0efbd3022bbe81834133c8c0d
unknown <skrm773@gmail.com>
2020-11-07 23:35:48 +0900

    同じ日付の肌ノートの投稿は上書き可能にした

--- app/controllers/user/notes_controller.rb
+++ app/controllers/user/notes_controller.rb
@@ -4,28 +4,29 @@ class User::NotesController < ApplicationController
 		@beginning_of_month = Date.new(params[:year].to_i, params[:month].to_i, 1)
 		@end_of_month = Date.new(params[:year].to_i, params[:month].to_i, -1)
 		@notes = current_user.notes.where(date: @beginning_of_month..@end_of_month)
-	
-		# {"悪い"=>13, "少し悪い"=>3, "普通"=>4, "良い"=>4, "とても良い"=>2}
-		grouping_conditions_count = @notes.group(:condition).count
 
-
+		# ↓{"悪い"=>13, "少し悪い"=>3, "普通"=>4, "良い"=>4, "とても良い"=>2}見たいなデータ
+		grouping_conditions_count = @notes.group(:condition).count #←何度も取りに行かなくていいようにgroupで取得する
 		@excellent_condition_count = grouping_conditions_count["とても良い"]
 		@good_condition_count = grouping_conditions_count["良い"]
 		@average_condition_count = grouping_conditions_count["普通"]
 		@poor_condition_count = grouping_conditions_count["少し悪い"]
 		@bad_condition_count = grouping_conditions_count["悪い"]
 
-		# @condition_list = (@beginning_of_month..@end_of_month).map do |date|
-  # 		{ x: date, y: @notes.find_by(date: date)&.read_attribute_before_type_cast(:condition) || 2 }
-  #   	end
-	 #    # ↑投稿がなかった日の肌のコンディションを「2:普通」にする
+		@condition_list = (@beginning_of_month..@end_of_month).map do |date| #mapは処理の結果が返ってくる(eachは繰り返したものの中身を返す)
+  		{ x: date, y: @notes.find_by(date: date)&.read_attribute_before_type_cast(:condition) || 2 }
+    	end
+	    # ↑投稿がなかった日の肌のコンディションを「2:普通」にする
 
-	 #    @todays_items_list = []
-		# current_user.my_items.pluck(:id).each do |my_item_id|
-		# 	(@beginning_of_month..@end_of_month).each do |date|
-		# 		@todays_items_list.push({ x: date, y: TodaysItem.joins(:note, :my_item).where(my_items: { user_id: current_user.id }, my_item_id: my_item_id).where('notes.date = ?', date).first&.my_item_id })
-		# 	end #.pushは()内の要素を配列の末尾に追加する .joinsは複数のテーブルにまたがるレコードを条件の一致するものだけくっつける
-		# end
+	    @my_item_lists_label = current_user.my_items.pluck(:id, :item_name).to_h.to_json
+	    # as_jsonはJSONに近いハッシュに変換してくれ、to_jsonはその更に先で完全に文字列化してくれる
+
+	    @todays_items_list = []
+		current_user.my_items.pluck(:id).each do |my_item_id|
+		 	(@beginning_of_month..@end_of_month).each do |date|
+		 		@todays_items_list.push({ x: date, y: TodaysItem.joins(:note, :my_item).where(my_items: { user_id: current_user.id }, my_item_id: my_item_id).where('notes.date = ?', date).first&.my_item_id })
+		 	end #.pushは()内の要素を配列の末尾に追加する .joinsは複数のテーブルにまたがるレコードを条件の一致するものだけくっつける
+		end
 	end
 
 	def new
@@ -37,19 +38,52 @@ class User::NotesController < ApplicationController
 	def create
 		params[:note][:user_id] = current_user.id
 		# ↑note_paramsで値が決定されるの前に書く
-		note = Note.new(note_params)
-		# if Note.find_by(date: params[:date], user_id: current_user.id)
-		# 	existing_note = Note.find_by(date: params[:date], user_id: current_user.id)
-		# 	existing_note.condition = note.condition
-		if note.save
-			todays_item_params[:my_item_id].each do |my_item_id|
-				TodaysItem.create(note_id: note.id, my_item_id: my_item_id)
-			end
-			redirect_to user_notes_path(year: Time.now.year, month: Time.now.month)
-		else
-			@my_items = MyItem.where(user_id: current_user.id)
-			render "new"
+		# note = Note.new(note_params)
+		date = Date.new(params[:note]["date(1i)"].to_i, params[:note]["date(2i)"].to_i, params[:note]["date(3i)"].to_i)
+
+		if Note.find_by(date: date, user_id: current_user.id)#新規だった場合はここを無視するようにifにする
+			old_note = Note.find_by(date: date, user_id: current_user.id)
+			old_note_ids = old_note.id
 		end
+
+		if old_note.present? #もし以前に登録したことがある日付だったら
+			if old_note.update(note_params)
+			# ここからTodaysItemのupdate--------------------------------------------------------------------------------
+				my_item_old_ids = TodaysItem.where(note_id: old_note_ids).pluck('my_item_id')
+				# ↑以前同じ日付に登録したTodaysItemのmy_item_idを取得
+				my_item_new_ids = todays_item_params[:my_item_id]
+				# 今登録されたTodaysItemのidを取得
+				if my_item_old_ids.blank? #　←もし前の投稿ではTodayItemを選択していなかったら
+					my_item_new_ids.each do | id |
+						TodaysItem.create(note_id: old_note.id, my_item_id: id)
+					end
+				elsif (my_item_old_ids - my_item_new_ids).size != 0 #もし前の投稿のTodayItemと違いがあったら([1,2,3]-[2,3]=[1])
+			    (my_item_old_ids - my_item_new_ids).each do | id | #今登録したTodaysItemになかったものを削除する
+			    	TodaysItem.find_by(note_id: old_note.id, my_item_id: id).delete
+			    end
+			    (my_item_new_ids - my_item_old_ids).each do | id |#以前登録したTodaysItemになかったものを登録する
+						TodaysItem.create(note_id: old_note.id, my_item_id: id)
+					end
+			# ここまでTodaysItemのupdate---------------------------------------------------------------------------------
+				end
+				redirect_to user_notes_path(year: Time.now.year, month: Time.now.month)
+			else
+				@my_items = MyItem.where(user_id: current_user.id)
+				render "new"
+			end
+		else
+			note = Note.new(note_params)
+
+			if note.save
+				todays_item_params[:my_item_id].each do |my_item_id|
+					TodaysItem.create(note_id: note.id, my_item_id: my_item_id)
+				end
+				redirect_to user_notes_path(year: Time.now.year, month: Time.now.month)
+			else
+				@my_items = MyItem.where(user_id: current_user.id)
+				render "new"
+			end
+	   end
 	end
 
 	def note_params
