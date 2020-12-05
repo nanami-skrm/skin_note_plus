5bd7b5f9d13879bb00bda0c9fc03769513a48cc6
unknown <skrm773@gmail.com>
2020-12-05 15:10:43 +0900

    vagrant上の画像アップデートエラー改善

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SkinNote
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    Refile.store ||= Refile::Backend::FileSystem.new("/tmp/uploads/store".to_s)
+    Refile.cache ||= Refile::Backend::FileSystem.new("/tmp/uploads/cache".to_s)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
